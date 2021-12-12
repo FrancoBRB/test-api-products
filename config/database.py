acdf93b2c404c9f85eaf98aebef0b99c595a2cf7
frankto5 <francojb5@hotmail.com>
2021-12-12 16:42:00 -0300

    Update database.py

--- config/database.py
+++ config/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.schema import MetaData
 
 
-DATABASE_URL = config('DB_URL')
+DATABASE_URL = config('DATABASE_URL')
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
